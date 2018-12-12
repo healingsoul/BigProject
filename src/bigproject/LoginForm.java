18981b49fabb225fab66c838b997ff4db55a0227
Alfaza <alfazasatria8@gmail.com>
2018-12-12 07:57:26 +0700

    test

--- src/bigproject/LoginForm.java
+++ src/bigproject/LoginForm.java
@@ -14,6 +14,7 @@ public class LoginForm extends javax.swing.JFrame {
     /**
      * Creates new form LoginForm
      */
+    private String name;
     public LoginForm() {
         initComponents();
     }
