e5268bae51785b2ae3ab499ff0efcf588ec860b2
Alfaza <alfazasatria8@gmail.com>
2018-12-18 21:51:12 +0700

    Class Koneksi Database

---
+++ src/bigproject/Lib/koneksi.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bigproject.Lib;
+
+/**
+ *
+ * @author Alfaza
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+ 
+public class koneksi {
+    public static Connection con;
+    public static Statement stm;
+    public static void main(String args[]){
+        try {
+            String url ="jdbc:mysql://localhost/bigproject";
+            String user="root";
+            String pass="";
+            Class.forName("com.mysql.jdbc.Driver");
+            con =DriverManager.getConnection(url,user,pass);
+            stm = con.createStatement();
+            System.out.println("koneksi berhasil;");
+        } catch (Exception e) {
+            System.err.println("koneksi gagal" +e.getMessage());
+        }
+    }
+    
+}
